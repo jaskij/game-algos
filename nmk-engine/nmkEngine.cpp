53a59ea1ec6e10f2e0b0d10c6af56c5213a32bed
Maciej Zimowski <zimek20@gmail.com>
2013-03-23 18:53:44 +0100

    Refactoring nmkEngine.cpp

--- nmk-engine/nmkEngine.cpp
+++ nmk-engine/nmkEngine.cpp
@@ -109,15 +109,11 @@ namespace engine
 
 
 	static const unsigned victoryDecidingFieldStates = NO_OF_PLAYERS + 1;//1 is for EMPTY field state
-	enum direction
-	{
-		HORIZONTAL,
-		VERTICAL
-	};
 
 	//finds if enough pieces are in a line to trigger a win for either player
+	template <typename coordFunc_t>
 	GameResult findSequence( const GameState* const State, unsigned counts[victoryDecidingFieldStates], 
-		const unsigned PRIMARY_DIMENSION, const unsigned SECONDARY_DIMENSION, direction DIRECTION )
+		const unsigned PRIMARY_DIMENSION, const unsigned SECONDARY_DIMENSION, coordFunc_t coordFunc )
 	{
 		FieldState current = EMPTY;
 		FieldState currentSequence = EMPTY;
@@ -125,15 +121,8 @@ namespace engine
 		{
 			for(unsigned i2 = 0; i2 < PRIMARY_DIMENSION; ++i2)
 			{
-				switch(DIRECTION)
-				{
-				case VERTICAL:
-					current = State->board[i2 * BOARD_W + i];
-					break;
-				case HORIZONTAL:
-					current = State->board[i * BOARD_H + i2];
-					break;
-				}
+				current = State->board[coordFunc(i,i2)];
+
 				counts[current]++;
 				if(currentSequence != current && currentSequence != EMPTY)
 					counts[currentSequence] = 0;
@@ -148,76 +137,62 @@ namespace engine
 		return NOT_FINISHED;
 	}
 
+	template <typename startPosCondition_t, typename diagonalXLimit_t>
+	GameResult findSequenceDiagonal( const GameState* const State, unsigned counts[victoryDecidingFieldStates], 
+		 CoordU start, startPosCondition_t startPosCondition, const int startXChange, diagonalXLimit_t diagonalXLimit)
+	{
+		FieldState current = EMPTY;
+		FieldState currentSequence = EMPTY;
+		if(start.y >= 0)//if there are any fitting diagonals(>=K)
+		{
+			//start is a starting coord for all diagonals with length >= K
+			// iteration starts at the bottom of left(or right) column, goes up and then right(or left)
+			for(; startPosCondition(start.x); start = start.y==0 ? CoordU(start.x + startXChange, 0) : CoordU(start.x, start.y-1))
+			{
+				unsigned diagonalLength = std::min(diagonalXLimit(start.x), BOARD_H - start.y);
+				assert(diagonalLength >= K);
+				for(unsigned i = 0; i < diagonalLength; ++i)
+				{
+					current = State->board[(start.y+i) * BOARD_W + (start.x+ startXChange * i)];
+					counts[current]++;
+					if(currentSequence != current && currentSequence != EMPTY)
+						counts[currentSequence] = 0;
+					currentSequence = current;
+					if(counts[P_1] == K)
+						return PLAYER_1;
+					if(counts[P_2] == K)
+						return PLAYER_2;
+				}
+				counts[P_1] = counts[P_2] = 0;
+			}
+		}
+		return NOT_FINISHED;
+	}
+
 	GameResult isGameFinished(const GameState* const State)
 	{
 		//this is a very naive implementation, but optimizing without moving to 2 bit fields seems pointless
-		// TODO: get rid of code duplication without losing performance(easier said then done). 
-		// also consider rotating the board instead of writing multiple functions
 		// NOTE: empty fields are being calculated multiple times, but since their value is only checked against zero 
 		// it isn't a problem. look into it in a future, optimized implementation
 		unsigned counts[victoryDecidingFieldStates] = {0};
 		
-		GameResult vertical = findSequence(State, counts, BOARD_H, BOARD_W, VERTICAL);
+		GameResult vertical = findSequence(State, counts, BOARD_H, BOARD_W, 
+			[](unsigned i, unsigned i2){return i2 * BOARD_W + i;});
 		if(vertical != NOT_FINISHED)
 			return vertical;
-		GameResult horizontal = findSequence(State, counts, BOARD_W, BOARD_H, HORIZONTAL);
+		GameResult horizontal = findSequence(State, counts, BOARD_W, BOARD_H, 
+			[](unsigned i, unsigned i2){return i * BOARD_H + i2;});
 		if(horizontal != NOT_FINISHED)
 			return horizontal;
 
-		FieldState current = EMPTY;
-		FieldState currentSequence = EMPTY;
-		CoordU start(0, BOARD_H - K);
-
-		if(start.y >= 0)//if there are any fitting diagonals(>=K)
-		{
-			//start is a starting coord for all diagonals with length >= K
-			// iteration starts at the bottom of left column, goes up and then right
-			for(; start.x <=  BOARD_W - K; start = start.y==0 ? CoordU(start.x+1, 0) : CoordU(start.x, start.y-1))
-			{
-				unsigned diagonalLength = std::min(BOARD_W - start.x, BOARD_H - start.y);
-				assert(diagonalLength >= K);
-				for(unsigned i = 0; i < diagonalLength; ++i)
-				{
-					current = State->board[(start.y+i) * BOARD_W + (start.x+i)];
-					counts[current]++;
-					if(currentSequence != current && currentSequence != EMPTY)
-						counts[currentSequence] = 0;
-					currentSequence = current;
-					if(counts[P_1] == K)
-						return PLAYER_1;
-					if(counts[P_2] == K)
-						return PLAYER_2;
-				}
-				counts[P_1] = counts[P_2] = 0;
-			}
-		}
-
-		current = EMPTY;
-		currentSequence = EMPTY;
-		start = CoordU(BOARD_W-1, BOARD_H - K);
-
-		if(start.y >= 0)//if there are any fitting diagonals(>=K)
-		{
-			//similar to previous loop, but starts in right column and searches downleft
-			for(; start.x >= K - 1; start = start.y==0 ? CoordU(start.x-1, 0) : CoordU(start.x, start.y-1))
-			{
-				unsigned diagonalLength = std::min(start.x + 1, BOARD_H - start.y);
-				assert(diagonalLength >= K);
-				for(unsigned i = 0; i < diagonalLength; ++i)
-				{
-					current = State->board[(start.y+i) * BOARD_W + (start.x-i)];
-					counts[current]++;
-					if(currentSequence != current && currentSequence != EMPTY)
-						counts[currentSequence] = 0;
-					currentSequence = current;
-					if(counts[P_1] == K)
-						return PLAYER_1;
-					if(counts[P_2] == K)
-						return PLAYER_2;
-				}
-				counts[P_1] = counts[P_2] = 0;
-			}
-		}
+		GameResult downright = findSequenceDiagonal(State, counts, CoordU(0, BOARD_H - K), 
+			[](unsigned x){return x <=  BOARD_W - K;}, 1, [] (unsigned x){return BOARD_W - x;});
+		if(downright != NOT_FINISHED)
+			return downright;
+		GameResult downleft = findSequenceDiagonal(State, counts, CoordU(BOARD_W-1, BOARD_H - K), 
+			[](unsigned x){return x >=  K - 1;}, -1, [] (unsigned x){return x+1;});
+		if(downleft != NOT_FINISHED)
+			return downleft;
 
 		if (counts[EMPTY] > 0)
 		{
