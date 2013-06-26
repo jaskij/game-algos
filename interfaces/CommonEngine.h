7941f91296d8d651e84a716e61d242fb66f08f3a
Maciej Zimowski <zimek20@gmail.com>
2013-06-26 20:29:20 +0200

    Mdified engine move generation API to be more flexible

--- interfaces/CommonEngine.h
+++ interfaces/CommonEngine.h
@@ -52,25 +52,27 @@ namespace engine
 
 	/*! \brief Generates moves, using GameState
 	initialized separately from genMovesComp, moves are also generated independently
+	\param buffer buffer for pointers to GameState objects
+	\param bufferSize size of the buffer
 	\return Number of moves generated
 	*/
-	unsigned genMovesUncomp();
+	unsigned genMovesUncomp(GameState* buffer[], const uint32_t bufferSize);
 
 	/*! \brief generates moves, using CompressedState
 	initialized separately from genMovesUncomp, moves are also generated independently
+	\param buffer buffer for pointers to GameState objects
+	\param bufferSize size of the buffer
 	\return number of moves generated
 	*/
-	unsigned genMovesComp();
+	unsigned genMovesComp(GameState* buffer[], const uint32_t bufferSize);
 
 	/*! \brief initializes the moves generator
 	due to differences in buffers, etc. engine operates separately on GameState and CompressedState
 	\param current what is the state of the board? expected to remain valid until generating moves is completed
-	\param buffer buffer for pointers to GameState objects
-	\param bufferSize size of the buffer
 	*/
-	void initGen(const GameState * const current, GameState* buffer[], const uint32_t bufferSize);
+	void initGen(const GameState * const current);
 	/// \overload due to differences in buffers, etc. engine operates separately on GameState and CompressedState
-	void initGen(const CompressedState * const current, CompressedState* buffer[], const uint32_t bufferSize);
+	void initGen(const CompressedState * const current);
 
 
 	/// \brief returns default state for current game, usually an empty board
